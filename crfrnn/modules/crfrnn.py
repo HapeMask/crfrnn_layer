8edd9ae17ca500779e8b71bf409f4adc3a1dac1b
Gabe Schwartz <gbschwartz@fb.com>
2020-11-11 19:36:51 -0500

    Update CRFRNN layer.

--- crfrnn/modules/crfrnn.py
+++ crfrnn/modules/crfrnn.py
@@ -9,17 +9,17 @@ from ..functions.gfilt import make_gfilt_buffers, make_gfilt_hash
 def gaussian_filter(ref, val, kstd, hb=None, gb=None):
     return gfilt(ref / kstd[:, None, None], val, hb, gb)
 
-def mgrid(h, w):
-    y = th.arange(0, h).repeat(w, 1).t()
-    x = th.arange(0, w).repeat(h, 1)
+def mgrid(h, w, dev):
+    y = th.arange(0, h, device=dev).repeat(w, 1).t()
+    x = th.arange(0, w, device=dev).repeat(h, 1)
     return th.stack([y, x], 0)
 
-def gkern(std, chans):
+def gkern(std, chans, dev):
     sig_sq = std**2
     r = sig_sq if (sig_sq % 2) else sig_sq-1
     s = 2*r + 1
-    k = th.exp(-((mgrid(s, s)-r)**2).sum(0) / (2*sig_sq))
-    W = th.zeros((chans, chans, s, s))
+    k = th.exp(-((mgrid(s, s, dev)-r)**2).sum(0) / (2*sig_sq))
+    W = th.zeros(chans, chans, s, s, device=dev)
     for i in range(chans):
         W[i, i] = k / k.sum()
     return W
@@ -33,7 +33,8 @@ class CRF(nn.Module):
                  compat_spatial=2, num_iter=5, normalize_final_iter=True,
                  trainable_kstd=False):
 
-        super(CRF, self).__init__()
+        super().__init__()
+
         self.sxy_bf = sxy_bf
         self.sc_bf = sc_bf
         self.compat_bf = compat_bf
@@ -42,60 +43,45 @@ class CRF(nn.Module):
         self.num_iter = num_iter
         self.normalize_final_iter = normalize_final_iter
 
-        if isinstance(sc_bf, (int,float)):
+        if isinstance(sc_bf, (int, float)):
             sc_bf = 3 * [sc_bf]
-        self.kstd = nn.Parameter(th.FloatTensor([sxy_bf, sxy_bf,
-                                                 sc_bf[0], sc_bf[1], sc_bf[2]]),
-                                 requires_grad=trainable_kstd)
+
+        kstd = th.FloatTensor([sxy_bf, sxy_bf, sc_bf[0], sc_bf[1], sc_bf[2]])
+        if self.trainable_kstd:
+            self.kstd = nn.Parameter(kstd)
+        else:
+            self.register_buffer("kstd", kstd)
 
     def forward(self, unary, ref):
-        is_cuda = (unary.is_cuda or ref.is_cuda)
-        cudev = None
-        if is_cuda:
-            assert(unary.is_cuda and ref.is_cuda)
-            assert(unary.get_device() == ref.get_device())
-            cudev = unary.get_device()
-
         N, ref_dim, H, W = ref.shape
         Nu, val_dim, Hu, Wu = unary.shape
         assert(Nu == N and Hu == H and Wu == W)
 
-        if not isinstance(unary, th.autograd.Variable):
-            unary = th.autograd.Variable(unary)
-        if not isinstance(ref, th.autograd.Variable):
-            ref = th.autograd.Variable(ref)
-
         if ref_dim not in [3, 1]:
-            raise ValueError("Reference image must be either color or greyscale \
-(3 or 1 channels).")
+            raise ValueError("Reference image must be either color or greyscale (3 or 1 channels).")
         ref_dim += 2
 
         kstd = self.kstd[:3] if ref_dim == 3 else self.kstd
-        gk = th.autograd.Variable(gkern(self.sxy_spatial, val_dim))
+        gk = gkern(self.sxy_spatial, val_dim, unary.device)
 
-        yx = mgrid(H, W)
+        yx = mgrid(H, W, unary.device)
         grid = yx[None].repeat(N, 1, 1, 1)
-        grid = th.autograd.Variable(grid, requires_grad=False)
 
-        if is_cuda:
-            gk, grid = [v.cuda(cudev) for v in [gk, grid]]
-
+        cap = get_hash_cap(H*W, ref_dim)
         stacked = th.cat([grid, ref], dim=1)
-        gb = make_gfilt_buffers(val_dim, H, W, get_hash_cap(H*W, ref_dim), is_cuda, cudev)
-        gb1 = make_gfilt_buffers(1, H, W, get_hash_cap(H*W, ref_dim), is_cuda, cudev)
+        gb = make_gfilt_buffers(val_dim, H, W, cap, unary.device)
+        gb1 = make_gfilt_buffers(1, H, W, cap, unary.device)
 
         def _bilateral(V, R, hb):
-            o = th.ones(1, H, W)
-            if is_cuda:
-                o = o.cuda(cudev)
+            o = th.ones(1, H, W, device=unary.device)
             norm = th.sqrt(gaussian_filter(R, o, kstd, hb, gb1)) + 1e-8
-            return gaussian_filter(R, V/norm, kstd, hb, gb) / norm
+            return gaussian_filter(R, V / norm, kstd, hb, gb) / norm
 
         def _step(prev_q, U, ref, hb, normalize=True):
             qbf = _bilateral(prev_q, ref, hb)
             qsf = th.nn.functional.conv2d(prev_q[None], gk, padding=gk.shape[-1]//2)[0]
 
-            q_hat = -self.compat_bf * qbf + -self.compat_spatial * qsf
+            q_hat = -self.compat_bf * qbf - self.compat_spatial * qsf
             q_hat = U - q_hat
 
             return softmax(q_hat, dim=0) if normalize else q_hat
